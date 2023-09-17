a4d1a378b08489051958e423b9722566e3f6a958
FabianoPereira <106458983+FabianoPereira1@users.noreply.github.com>
2023-09-17 13:34:46 -0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from views import CreateUserView,
+LoginUserView
 
+urlpatterns = [
+
+  path('create/',CreateUserView.as_view()),
+
+  path('login/',LoginUserView.as_view()),
+]
