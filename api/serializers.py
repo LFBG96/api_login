eb383e820d33bd2b8ecd701333177c0b9912656c
omateusguerra <111542141+omateusguerra@users.noreply.github.com>
2023-09-18 16:27:37 -0300

    Adição de classe login

--- api/serializers.py
+++ api/serializers.py
@@ -15,3 +15,26 @@ class CreateUserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+class LoginUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    class Meta:
+        model = User
+        fields = ('username','password')
+
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password") 
+        user = User.objects.filter(username=username).first()
+        if not user:
+            raise serializers.ValidationError({"username":"Usuario invalido"})
+        if not user.check_password(password):
+            raise serializers.ValidationError({"password":"Senha invalida"})
+            
+        return super().validate(attrs)
+
+    def create(self,validate_data):
+        username = validate_data.get("username")
+        user = User.objects.filter(username=username).first()
+        return user
