267bd2bf8474a3b7891b2d1e9ec6d1416a125f59
danielfsantoss <danielfigueiredo009@gmail.com>
2023-09-18 16:59:42 -0300

    views.py adicionado.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from .serializers import CreateUserSerializer, LoginUserSerializer
 
-# Create your views here.
+class CreateUserView(CreateAPIView):
+    serializer_class = CreateUserSerializer
