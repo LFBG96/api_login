156008e48903a3fe3b7cfca5e2e9eee567e1358c
lfbg96 <lf5154223@gmail.com>
2023-11-15 13:49:16 -0300

    add CORS

--- core/settings.py
+++ core/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
